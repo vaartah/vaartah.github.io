d170f8aa068d7208a14e4c9b8fa233073bdf1ade
Chandra Mohan Jha <chandramohanjha@hotmail.com>
2019-06-12 13:09:22 +0530

    updated js disabled UI

---
+++ precache-manifest.4ffdada0f7859f260f8b843ddd715225.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0210f5020e624d6c55ea1aaa84850c4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ffaa444c32e7f2a339da",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "ffaa444c32e7f2a339da",
+    "url": "/static/js/main.85672ebe.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
